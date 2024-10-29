2faa50d053af8c086547630ca923242e03854555
Richard-Alves167 <riichardd.08@gmail.com>
2024-10-28 22:07:49 -0300

    Atualização cadastros
    Melhorias no CSS do cadastro de itens e de compradores, além de melhoria no Javascript e mudança  dos produtos da tabela para card.

--- projeto/Cadastramento/javascript/pegar_itens.js
+++ projeto/Cadastramento/javascript/pegar_itens.js
@@ -1,26 +1,32 @@
 let novatag = document.createElement("li")
 
 function cadastrar() {
-    const nome_iten = document.getElementById("nome_itens").value
-    const preco_iten = document.getElementById("preco_itens").value
-    const tipo_de_artigo = document.getElementById("seletor_tipo_artigo").children[document.getElementById("seletor_tipo_artigo").selectedIndex].textContent
-    const nivel_puericultura = document.querySelector('input[name="nivel"]:checked').value
-    const nacional_internacional = document.getElementById("seletor_nacionalidade").children[document.getElementById("seletor_nacionalidade").selectedIndex].textContent
+    const iten = {
+    nome: document.getElementById("nome_itens").value,
+    preco: document.getElementById("preco_itens").value,
+    imagem: document.getElementById("imagem_itens").value,
+    tipoDeArtigo: document.getElementById("seletor_tipo_artigo").children[document.getElementById("seletor_tipo_artigo").selectedIndex].textContent,
+    nivelPuericultura: document.querySelector('input[name="nivel"]:checked').value,
+    localidade: document.getElementById("seletor_nacionalidade").children[document.getElementById("seletor_nacionalidade").selectedIndex].textContent,
+    }
 
-    let lista = document.getElementById("colocar_cadastro")
+    let lista = document.getElementById("itens_cadastrados")
     
-    let novotr = document.createElement("tr")
+    let novotr = document.createElement("li")
 
     lista.appendChild(novotr)
 
     novotr.innerHTML = `
-    <td>${nome_iten}</td>
-    <td>R$ ${preco_iten}</td>
-    <td>${tipo_de_artigo}</td>
-    <td>${nivel_puericultura}</td>
-    <td>${nacional_internacional}</td>
+    <li class="card">
+    <div class="itens_cadastrados">
+    <img class="iten_imagem" src="${iten.imagem}">
+    <div class="iten_nome">${iten.nome}</div>
+    <div class="iten_preco"><span class="cifrao">R$</span>${iten.preco}</div>
+    </div>
+    </li>
     `
 
     document.getElementById("nome_itens").value = null
     document.getElementById("preco_itens").value = null
+    document.getElementById("imagem_itens").value = null
 }
